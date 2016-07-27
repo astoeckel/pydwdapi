5e3eebf88ce696d9cfa21c5b92dc8053e0c11e3e
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-07-27 13:26:55 +0200

    Correctly decode HTML entities

--- pydwdapi/html_table_parser.py
+++ pydwdapi/html_table_parser.py
@@ -20,11 +20,10 @@ class HTMLTableParser(HTMLParser):
     tables which you feed in. You can access the result per .tables field.
     """
 
-    def __init__(self, decode_html_entities=False, data_separator=' ', ):
+    def __init__(self, data_separator=' '):
 
-        HTMLParser.__init__(self)
+        HTMLParser.__init__(self, convert_charrefs=True)
 
-        self._parse_html_entities = decode_html_entities
         self._data_separator = data_separator
 
         self._in_td = False
@@ -47,12 +46,6 @@ class HTMLTableParser(HTMLParser):
         """ This is where we save content to a cell """
         if self._in_td or self._in_th:
             self._current_cell.append(data.strip())
-
-    def handle_charref(self, name):
-        """ Handle HTML encoded characters """
-
-        if self._parse_html_entities:
-            self.handle_data(self.unescape('&#{};'.format(name)))
 
     def handle_endtag(self, tag):
         """ Here we exit the tags. If the closing tag is </tr>, we know that we
